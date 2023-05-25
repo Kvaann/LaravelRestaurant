46c38efef0818c557a3db524eb052802f207ae7a
Kvaann <128744312+Kvaann@users.noreply.github.com>
2023-05-25 08:20:15 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,53 @@ Route::get('/', function () {
     return view('mainwindow');
 });
 
+// Route::get('/menu', [MenuController::class, 'index']);
+
+
+// Route::get('/aboutus', [AboutController::class, 'index']);
+
+Route:controller(ContactController:class->group(function (){
+    Route::get('/contact', [ContactController::class, 'index']);
+    Route::post('/contact', [ContactController::class, 'send_email'])->name('sendEmail');
+});
+
+
+// Route::get('/gallery', [GalleryController::class, 'index']);
+
 use App\Http\Controllers\MenuController;
-Route::get('/menu', [MenuController::class, 'index']);
-
 use App\Http\Controllers\AboutController;
-Route::get('/aboutus', [AboutController::class, 'index']);
-
-use App\Http\Controllers\ContactController;
-Route::get('/contact', [ContactController::class, 'index']);
-Route::post('/contact', [ContactController::class, 'send_email'])->name('sendEmail');
-
 use App\Http\Controllers\GalleryController;
-Route::get('/gallery', [GalleryController::class, 'index']);
+Route::get('/', function () {
+    return view('index');
+});
 
-use App\Http\Controllers\LoginController;
-Route::get('/login', [LoginController::class, 'index']);
-Route::post('/login', [LoginController::class, 'login'])->name('logUser');
+Route:controller(LoginController:class->group(function (){
+    Route::get('/login', [LoginController::class, 'index']);
+    Route::post('/login', [LoginController::class, 'login'])->name('logUser');
+});
 
-use App\Http\Controllers\RegisterController;
-Route::get('/register', [RegisterController::class, 'index']);
-Route::post('/register', [RegisterController::class, 'AddUser'])->name('updateUser');
+Route:controller(RegisterController:class->group(function (){
+    Route::get('/register', [RegisterController::class, 'index']);
+    Route::post('/register', [RegisterController::class, 'AddUser'])->name('updateUser');
+});
 
 use App\Http\Controllers\LogoutController;
 Route::get('/logout', [LogoutController::class, 'index'])->name('logoutUser');
 
 use App\Http\Controllers\ReservationController;
 Route::resource('reservation', ReservationController::class);
+
+
+
+// use App\Http\Controllers\ContactController;
+// Route::get('/contact', [ContactController::class, 'index']);
+// Route::post('/contact', [ContactController::class, 'send_email'])->name('sendEmail');
+
+// use App\Http\Controllers\LoginController;
+// Route::get('/login', [LoginController::class, 'index']);
+// Route::post('/login', [LoginController::class, 'login'])->name('logUser');
+
+// use App\Http\Controllers\RegisterController;
+// Route::get('/register', [RegisterController::class, 'index']);
+// Route::post('/register', [RegisterController::class, 'AddUser'])->name('updateUser');
 
